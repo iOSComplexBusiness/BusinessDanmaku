91580c9c12f02f67517c2788f7fb452f854f2dbd
zhaoyang <wustzhy@sina.com>
2025-09-28 22:22:26 +0800

    feat: 添加Target类和action方法

--- BusinessDanmaku/BusinessDanmaku/Targets/Target_BusinessDanmaku.h
+++ BusinessDanmaku/BusinessDanmaku/Targets/Target_BusinessDanmaku.h
@@ -5,11 +5,13 @@
 //  Created by yestin ✨ on 9/28/25.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface Target_BusinessDanmaku : NSObject
+
+//- (UIViewController *)Action_viewController:(NSDictionary *)params;
 
 @end
 
--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment this line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '9.0'
 
 source 'https://github.com/iOSComplexBusiness/PrivatePods'
 source 'https://github.com/CocoaPods/Specs.git'
