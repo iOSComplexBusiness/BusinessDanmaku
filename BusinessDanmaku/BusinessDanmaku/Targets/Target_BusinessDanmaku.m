69ba53d4741a89ea0fc232c2549ae43a8b41cfed
zhaoyang <wustzhy@sina.com>
2025-09-28 22:22:26 +0800

    feat: 添加Target类和action方法

--- BusinessDanmaku/BusinessDanmaku/Targets/Target_BusinessDanmaku.m
+++ BusinessDanmaku/BusinessDanmaku/Targets/Target_BusinessDanmaku.m
@@ -6,7 +6,17 @@
 //
 
 #import "Target_BusinessDanmaku.h"
+#import "DanmakuViewController.h"
 
 @implementation Target_BusinessDanmaku
+
+- (UIViewController *)Action_viewController:(NSDictionary *)params
+{
+    DanmakuViewController *viewController = [[DanmakuViewController alloc] init];
+    if (params[@"title"]) {    
+        viewController.title = params[@"title"];
+    }
+    return viewController;
+}
 
 @end
